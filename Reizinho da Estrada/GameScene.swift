e62d240768baf370fdb14311f6e388b11782b79c
Fábio Pereira <fabio.pereira@mv.com.br>
2019-08-11 22:05:10 -0300

    first commit

--- Reizinho da Estrada/GameScene.swift
+++ Reizinho da Estrada/GameScene.swift
@@ -11,62 +11,58 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var player : SKSpriteNode? = nil
+    private var background: SKSpriteNode?
+    private let backgroudImageName = "fundo"
+    private let playerImageName = "nick"
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    fileprivate func setupBackground() {
+        background = SKSpriteNode(texture: SKTexture(imageNamed: backgroudImageName))
+        self.addChild(background!)
     }
     
+    fileprivate func setupPlayer() {
+        let widthValue = 170
+        let heigthValue = 150
+        let xValue = -305
+        let yValue = -115
+        
+        var playerImageFrames : [SKTexture] = []
+        
+        for i in 1...2 {
+            playerImageFrames.append(SKTexture(imageNamed: "\(playerImageName)\(i)"))
+        }
+        
+        let animationAction = SKAction.animate(with: playerImageFrames, timePerFrame: 0.3)
+        let repeatAction = SKAction.repeatForever(animationAction)
+        
+        player = SKSpriteNode(imageNamed: "\(playerImageName)1")
+        player?.size = CGSize(width: widthValue, height: heigthValue)
+        player?.position = CGPoint(x: xValue, y: yValue)
+        player?.run(repeatAction)
+        self.addChild(player!)
+    }
+    
+    override func didMove(to view: SKView) {
+        setupBackground()
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+        if player == nil {
+            setupPlayer()
         }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
